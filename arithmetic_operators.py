d634ee63b1f94aaaedc8a9d1b2e976f8540304c7
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-14 14:29:20 +0000

    arithmetic_operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,15 @@
+# Arithmetic Operators
+    # addition : +
+    # subtraction : -
+    # multiplication : *
+    # division : /
+    # exponent : **
+    # modulus : %
+    # floor division : //
+
+a = 10
+b = 3
+
+total = a + b, a - b, a * b, a / b, a ** b, a % b, a // b
+
+print(total)
