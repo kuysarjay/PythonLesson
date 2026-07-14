d9447266e9988583d27d86a3c6d3eee590600882
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-14 14:43:12 +0000

    comparison_operators

---
+++ comparison_operators.py
@@ -0,0 +1,14 @@
+# Comparison Operators - this compares values with the answer of TRUE or FALSE
+    # equal to : ==
+    # not equal to : !=
+    # greater than : >
+    # less than : <
+    # greater than or equal to : >=
+    # less than or equal to : <=
+    
+a = 10
+b = 20
+
+compare = a == b, a != b, a > b, a < b, a >= b, a <= b
+
+print (compare)
