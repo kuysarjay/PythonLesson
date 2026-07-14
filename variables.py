8479f71eab45f8a4d5abed159ff91035ff954c0f
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-14 13:40:33 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Variables - is a container of data types, temporary memories
+    # user - define : name
+    # case - sensitive
+    # starts - letter _, not with : numbers or special character
+    # mutable - changable
+    # dynamic - not fixed
+    
+number_one = 10
+number_two = 50
+
+print(number_one + number_two)
