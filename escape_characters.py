b47c64e4abdf3d97f8612bcc46bd1b686f9e56d7
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-14 22:44:26 +0000

    escape_characters

---
+++ escape_characters.py
@@ -0,0 +1,6 @@
+# Escape Characters : insert characters inside in a string that would otherwise be difficult to include, 
+#  as quotes or newlines.
+print("He said, \"Hello, world!\"")
+print("This is a new line.\nThis is the second line.")
+print("This is a tabbed line.\tThis is after the tab.")
+print("This is a backslash: \\")
