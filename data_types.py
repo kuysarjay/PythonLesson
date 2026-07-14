49571e8d063ff5ff6fb47bb79dade0bf92a6bd00
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-14 13:28:24 +0000

    data_types

---
+++ data_types.py
@@ -0,0 +1,12 @@
+# hashtag is use for comment in python programming
+
+# Data Types:
+    # string - is a letter, special character or number with double quote or ""
+        #eg.: "Hello World!!", "KuysArjay", "09123456789"
+    # integers or int - is a whole number without decimal value
+        #eg.: 1, 2, 3 or 1 + 1, 10 - 1
+    # float - is a number with decimal value
+        #eg.: 3.1416
+    # boolean - with a value of TRUE or FALSE
+    
+print(10)
