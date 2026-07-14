c4aba56d5a0ceebc31566ee50bd03405b2eaca27
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-14 15:45:49 +0000

    assignment_operators

---
+++ assignment_operators.py
@@ -0,0 +1,19 @@
+# Assignment Operators - used to assign values to variables
+    # assignment operator: =, +=, -=, *=, /=, %=, **=, //=
+
+a = 5
+a += 3  # a = a + 3
+b = 10
+b -= 2  # b = b - 2
+c = 4
+c *= 2  # c = c * 2
+d = 20
+d /= 4  # d = d / 4
+e = 15
+e %= 4  # e = e % 4
+f = 2
+f **= 3  # f = f ** 3
+g = 10
+g //= 3  # g = g // 3
+
+print(a, b, c, d, e, f, g)  
