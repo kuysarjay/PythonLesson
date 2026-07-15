4f82dabf99b43bd0069856441cc636173f24eecd
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-15 13:17:36 +0000

    if_statement

---
+++ if_statement.py
@@ -0,0 +1,15 @@
+# If statement is a conditional statement that allows you to execute a block of code 
+    # only if a specified condition is TURE. 
+    # It is commonly used in programming to control the flow of execution based on certain criteria.
+# Structure of an if statement:
+# if condition:
+    # block of code to be executed if the condition is TRUE, 
+    # if the condition is FALSE, block of code will be skipped.
+
+# Example of an if statement in Python:
+
+age = int(input("Enter your age: "))  # Taking user input for age and converting it to an integer.
+
+
+if age >= 18:
+    print("You are eligible to vote.")  # This line will be executed because the condition is TRUE.
