a313f5663afb12979f321eec929f8ac5d7bcefbe
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-15 14:39:21 +0000

    while_loop

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+# While Loops is a control flow statement that allows code to be executed repeatedly based on a given Boolean condition.
+# The while loop can be thought of as a repeating if statement.
+
+# Structure of a while loop:
+# while condition:
+    # block of code to be executed repeatedly as long as the condition is TRUE.
+
+# Example of a while loop in Python:
+count = 0  # Initialize a counter variable.
+while count <= 5:  # The loop will continue as long as the condition is TRUE
+    print(f"Count is: {count}")  # Print the current value of count.
+    count += 1  # Increment the counter variable by 1.
