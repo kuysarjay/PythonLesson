da4e4fe7ecfa53d88d06569caf5003c4e5b86ee1
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-16 01:16:17 +0000

    for_loop_part2

---
+++ for_loop_part2.py
@@ -0,0 +1,11 @@
+# For Loop, loop thru string and Lists.
+
+# Example of a for loop thru string in Python:
+word = "Python"
+for letter in word:  # Start a for loop that will iterate through each letter in the string "Python".
+    print(letter)  # Print each letter on a new line.
+
+# Example of a for loop thru list in Python:
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:  # Start a for loop that will iterate through each item in the list "fruits".
+    print(fruit)  # Print each fruit on a new line.
