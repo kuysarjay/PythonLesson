cce042007ab75e32622fde5d801fcfa3b411cdec
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-16 01:01:36 +0000

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+# For Loop is a control flow statement that allows code to be executed repeatedly for a fixed number of iterations. It is commonly used to iterate over a sequence (like a list, tuple, or string) or to perform an action a specific number of times.
+# For Loop can be thought of as a repeating block of code that executes for each item in a sequence or for a specified range of numbers.
+# For Loop is determined by the number of items in the sequence or the range specified, and it continues until all items have been processed or the range is exhausted.
+    # range(start, stop, step) is a built-in function that generates a sequence of numbers. It can be used in a for loop to specify the number of iterations.
+
+# Example of a for loop in Python:
+for i in range(1, 6):  # Start a for loop that will iterate 5 times (from 0 to 4).
+    print("*" * i)  # Print a string of asterisks, increasing in length with each iteration.
